c2ee2552de30d3f1852fc63c381a3bda43d96a75
Mahmudul Hassan <93487707+MahmudHassanLalin@users.noreply.github.com>
2024-01-30 23:17:56 -0600

    Form Validation, Change Url n about section and contact section

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -15,7 +15,7 @@ export class ContactComponent implements OnInit {
   ngOnInit(): void {
     this.messageSendForm = new FormGroup({
       Name: new FormControl(null, [Validators.required]),
-      From: new FormControl(null, [Validators.required]),
+      From: new FormControl(null, [Validators.required,Validators.email]),
       Subject: new FormControl(null, [Validators.required]),
       Body: new FormControl(null, [Validators.required]),
     });
--- src/app/experience/experience.service.ts
+++ src/app/experience/experience.service.ts
@@ -7,18 +7,22 @@ import { experience } from './model/experience';
 export class ExperienceService {
 
   experienceList:experience[]=[
-    new experience("March, 2021","January 2023","Senior Software Engineer","Kaz Software",
-    "Dhaka, Bangaladesh","•	Analyzed, designed and developed a project framework from a legacy Java monolith application into an ASP.NET application using microservices architecture by implementing AWS Serverless architecture. It optimized the application by 30% and reduced the server cost by 25%."+
-    "<br>•	Updated, modified, and expanded existing web applications using C#, ASP.NET MVC, and Azure DevOps"+
-    "<br>•	Designed and developed a front-end application using React and Angular with typescript, jQuery, JavaScript, and Bootstrap."+
-    "<br>•	Planned sprints and conducted day-to-day scrum meetings as a scrum master."+
-    "<br>•	Contributed to all phases of the development lifecycle."+
-    "<br>•	Updated, modified, and expanded existing web applications using C# and ASP.NET Core"+
-    "<br>•	Motivated team members and created an inspiring team environment with an open communication culture."),
-    new experience("September 2016","February 2021","Assistant Manager",
+    new experience("March, 2021","January 2023","Senior Software Engineer/Team Lead","Kaz Software",
+    "Dhaka, Bangaladesh","Analyzed, designed and developed a project framework from a legacy Java monolith application into an ASP.NET application using microservices architecture."+
+    " Updated, modified, and expanded existing web applications using C#, ASP.NET MVC, and Azure DevOps."+
+    " Planned sprints and conducted day-to-day scrum meetings as a scrum master."),
+    new experience("October 2019","February 2021","Assistant Manager",
+    "MISAMI-BITPI Group","Dhaka, Bangladesh","Enhanced, and modified existing enterprise level web and desktop-based applications using C#, ASP.NET Core, and ASP.NET MVC."+
+    " Wrote stored procedures, user define functions, views, triggers, and complex SQL Queries in SQL Server"),
+    new experience("October 2017","September 2019","Senior Programmer",
+    "MISAMI-BITPI Group","Dhaka, Bangladesh"),
+    new experience("September 2016","September 2017","Programmer",
     "MISAMI-BITPI Group","Dhaka, Bangladesh"),
     new experience("May 2013","August 2016","Software Engineer","Global Software Architects",
-    "Dhaka, Bangladesh")
+    "Dhaka, Bangladesh","Wrote server-side code using ASP.NET MVC and ASP.NET Web Forms."+
+    " Wrote store procedures and triggers using MS SQL Server."+
+    " Worked intensely on the front-end using jQuery, bootstrap, and the Google Maps API."+
+    " Developed and maintained mobile applications using Xamarin for Android and iOS.")
   ];
     constructor() { 
   
